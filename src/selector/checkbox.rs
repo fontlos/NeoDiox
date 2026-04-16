7b20a37ce8d56be108aa3c6133277ee67800c717
Fontlos <fontlos@fontlos.com>
2026-04-16 15:14:13 +0800

    refactor: Simplify `Checkbox`

--- src/selector/checkbox.rs
+++ src/selector/checkbox.rs
@@ -1,59 +1,49 @@
 use dioxus::prelude::*;
 
+use crate::icon;
+
 /// Checkbox
 #[derive(Props, PartialEq, Clone)]
 pub struct CheckboxProps {
+    /// Custom class name
+    #[props(default)]
+    pub class: Option<String>,
+
     /// Whether it is checked
     #[props(default)]
     pub checked: bool,
     /// Whether it is disabled
     #[props(default)]
     pub disabled: bool,
+
     /// Change event
-    pub on_change: EventHandler<bool>,
-    /// Custom class name
-    #[props(default)]
-    pub class: Option<String>,
+    pub onchange: EventHandler<bool>,
 }
 
 /// Checkbox component
 #[component]
 pub fn Checkbox(props: CheckboxProps) -> Element {
     let class = props.class.unwrap_or_default();
-
-    let active = if props.checked {
-        "nd-checkbox-active"
-    } else {
-        ""
-    };
-
-    let disabled_style = if props.disabled {
-        "opacity: 0.6; cursor: not-allowed;"
-    } else {
-        ""
-    };
-
-    // Checkmark opacity animation
-    let check_opacity = if props.checked { "1" } else { "0" };
+    let disabled = props.disabled;
+    let checked = props.checked;
 
     rsx! {
-        button {
-            r#type: "button",
-            role: "checkbox",
-            "aria-checked": if props.checked { "true" } else { "false" },
-            disabled: if props.disabled { "true" } else { "false" },
-            class: "nd-checkbox {active} {class}",
-            style: disabled_style,
-            onclick: move |_| {
-                if !props.disabled {
-                    props.on_change.call(!props.checked);
-                }
-            },
-            // Checkmark icon - opacity transition
-            span {
+        div {
+            class: "nd-checkbox {class}",
+            icon::Icon {
                 class: "nd-checkbox-icon",
-                style: format!("opacity: {};", check_opacity),
-                "✓"
+                size: 12,
+                color: "white",
+                icon::Checked { }
+            }
+            input {
+                r#type: "checkbox",
+                checked,
+                disabled,
+                class: "nd-checkbox-input",
+                onchange: move |_| {
+                    props.onchange.call(!checked);
+                },
             }
         }
     }
