9109e8c8cfe70170ae9217de2c97910c9a81af42
Fontlos <fontlos@fontlos.com>
2026-05-24 14:39:53 +0800

    refactor: Simplify `Dropdown`

--- src/selector/dropdown.rs
+++ src/selector/dropdown.rs
@@ -13,17 +13,18 @@ pub struct DropdownProps<T: Display + PartialEq + Clone + 'static> {
 
     /// Dropdown Options
     pub options: Vec<T>,
-    /// Current selected value
-    #[props(default)]
-    pub value: Option<T>,
-    /// Change event
-    pub onchange: EventHandler<T>,
     /// Placeholder text
     #[props(default)]
     pub placeholder: Option<String>,
     /// Whether to enable search
     #[props(default)]
     pub searchable: bool,
+    /// Current selected value
+    #[props(default)]
+    pub value: Option<T>,
+
+    /// Change event
+    pub onchange: EventHandler<T>,
 }
 
 /// Dropdown Component
@@ -40,7 +41,7 @@ pub struct DropdownProps<T: Display + PartialEq + Clone + 'static> {
 ///         value: country,
 ///         onchange: move |val| set_country(val),
 ///         searchable: true,
-///         placeholder: "Search countries...".to_string(),
+///         placeholder: "Search countries...",
 ///     }
 /// }
 /// ```
@@ -53,83 +54,77 @@ pub fn Dropdown<T: Display + PartialEq + Clone + 'static>(props: DropdownProps<T
         props.value.as_ref().map(|v| v.to_string()).unwrap_or_default()
     });
 
-    rsx! {
-        div {
-            class: "nd-dropdown {class}",
-            style: "position: relative;",
-
-            div { class: "nd-dropdown-input-wrapper",
-                input {
-                    class: "nd-dropdown-input nd-surface-inset nd-shadow-inset nd-text",
-                    value: "{input}",
-                    placeholder: props.placeholder,
-                    readonly: !props.searchable,
-                    autocomplete: "off",
-                    onclick: move |_| is_open.set(true),
-                    onblur: move |_| is_open.set(false),
-                    oninput: move |evt| input.set(evt.value()),
-                    onkeydown: move |evt| {
-                        if evt.key() == Key::Escape {
-                            is_open.set(false);
-                        }
-                    },
-                }
-
-                // 下拉箭头
-                icon::Icon {
-                    class: if is_open() { "nd-dropdown-arrow nd-dropdown-arrow-open" } else { "nd-dropdown-arrow" },
-                    size: 16,
-                    icon::Arrow {}
-                }
+    let search = input().to_lowercase();
+    let filtered: Vec<_> = props.options
+        .iter()
+        .filter(|o| {
+            if props.searchable && !search.is_empty() {
+                return o.to_string().to_lowercase().contains(&search);
             }
+            true
+        })
+        .cloned()
+        .collect();
+    let menu = if is_open() {
+        rsx! {
+            div {
+                class: "nd-dropdown-menu nd-surface nd-shadow",
+                onmousedown: |e| {
+                    // 阻止冒泡, 防止选项点击还没触发就触发了 blur 导致菜单关闭
+                    e.prevent_default();
+                    e.stop_propagation();
+                },
 
-            // 下拉列表
-            if is_open() {
-                {
-                    let search = input.read().clone();
-                    let filtered: Vec<_> = props.options
-                        .iter()
-                        .filter(|o| {
-                            if props.searchable && !search.is_empty() {
-                                return o.to_string().to_lowercase().contains(&search.to_lowercase());
+                for option in filtered {
+                    button {
+                        class: "nd-dropdown-item",
+                        "data-selected": Some(&option) == props.value.as_ref(),
+                        onclick: {
+                            move |_| {
+                                input.set(option.to_string());  // 直接覆盖
+                                props.onchange.call(option.clone());
+                                is_open.set(false);
                             }
-                            true
-                        })
-                        .cloned()
-                        .collect();
-
-                    rsx! {
-                        div {
-                            class: "nd-dropdown-menu",
-                            onmousedown: |e| {
-                                // 阻止冒泡, 防止选项点击还没触发就触发了 blur 导致菜单关闭
-                                e.prevent_default();
-                                e.stop_propagation();
-                            },
-
-                            for option in filtered {
-                                button {
-                                    class: if Some(&option) == props.value.as_ref()
-                                        { "nd-dropdown-item selected" }
-                                        else { "nd-dropdown-item" },
-                                    onclick: {
-                                        let option = option.clone();
-                                        let onchange = props.onchange.clone();
-                                        let mut is_open = is_open.clone();
-                                        let mut input_value = input.clone();
-                                        move |_| {
-                                            input_value.set(option.to_string());  // 直接覆盖
-                                            onchange.call(option.clone());
-                                            is_open.set(false);
-                                        }
-                                    },
-                                    "{option}"
-                                }
-                            }
-                        }
+                        },
+                        "{option}"
                     }
                 }
             }
+        }
+    } else {
+        rsx! {}
+    };
+
+    rsx! {
+        div {
+            class: "nd-dropdown {class}",
+            "data-open": is_open,
+
+            input {
+                class: "nd-dropdown-input nd-surface-inset nd-shadow-inset nd-text",
+                value: "{input}",
+                placeholder: props.placeholder,
+                readonly: !props.searchable,
+                autocomplete: "off",
+                onclick: move |_| is_open.set(true),
+                onblur: move |_| is_open.set(false),
+                oninput: move |evt| input.set(evt.value()),
+                onkeydown: move |evt| {
+                    if evt.key() == Key::Escape {
+                        is_open.set(false);
+                    }
+                },
+            }
+
+            // 下拉箭头
+            icon::Icon {
+                class: "nd-dropdown-arrow",
+                size: 16,
+                icon::Arrow {}
+            }
+
+            // 下拉列表
+            { menu }
         }
     }
 }
