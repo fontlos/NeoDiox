4d39ab903d6b3a95b67570bb71ddd5982f20ffc8
Fontlos <fontlos@fontlos.com>
2026-06-13 19:51:04 +0800

    refactor: Simplify `Spinner`

--- src/feedback/spinner.rs
+++ src/feedback/spinner.rs
@@ -3,18 +3,12 @@ use dioxus::prelude::*;
 /// Spinner Loading
 #[derive(Props, PartialEq, Clone)]
 pub struct SpinnerProps {
-    /// Size (pixels)
-    #[props(default = 24)]
-    pub size: u32,
-    /// Color
-    #[props(default = "#7c3aed".to_string())]
-    pub color: String,
-    /// Border width
-    #[props(default = 2)]
-    pub border_width: u32,
     /// Custom class name
     #[props(default)]
     pub class: Option<String>,
+    /// Size (pixels)
+    #[props(default = 32)]
+    pub size: u32,
 }
 
 /// Spinner component
@@ -23,24 +17,18 @@ pub struct SpinnerProps {
 ///
 /// ```rust,ignore
 /// rsx! {
-///     Spinner {
-///         size: 32,
-///         color: "#7c3aed".to_string(),
-///     }
+///     Spinner { }
 /// }
 /// ```
 #[component]
 pub fn Spinner(props: SpinnerProps) -> Element {
     let class = props.class.unwrap_or_default();
-
+    let size = props.size;
     rsx! {
         div {
             class: "nd-spinner {class}",
             "aria-label": "Loading",
-            style: format!(
-                "width: {}px; height: {}px; border-width: {}px; color: {};",
-                props.size, props.size, props.border_width, props.color
-            ),
+            style: "width: {size}px; height: {size}px;",
         }
     }
 }
